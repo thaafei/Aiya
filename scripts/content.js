15c52c90490b82599586302f4ddeb8de216a3bf4
Fei <50682656+thaafei@users.noreply.github.com>
2025-06-29 16:57:57 -0400

    Update script

--- scripts/content.js
+++ scripts/content.js
@@ -3,8 +3,11 @@ function renderRemoveContent(yuckyAI) {
   if (!yuckyAI) {
     return;
   }
-  console.log("AI content found")
-  yuckyAI.remove()
+  console.log("AI overview found: ZAP!")
+  sparrow = document.createElement("div")
+  sparrow.innerText = "AI Overview Removed Sir! (￣^￣ )ゞ"
+  parent = yuckyAI.parentElement
+  parent.replaceChildren(sparrow)
 }
 
-renderRemoveContent(document.querySelector('[data-hveid="CAcQBg"]'));
+renderRemoveContent(document.querySelector('[data-mcp], [data-mcpr]'));
